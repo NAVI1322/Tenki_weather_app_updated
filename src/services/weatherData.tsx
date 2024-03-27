b48cbb825f2193bd5f0a5b6962d4acc78e689aa4
Gurtaj06 <sonicgurtaj079@gmail.com>
2024-03-27 19:00:56 -0400

    connected to openweatherApi

---
+++ src/services/weatherData.tsx
@@ -0,0 +1,25 @@
+const API_KEY = import.meta.env.VITE_WEATHER_API
+const BASE_URL = "https://pro.openweathermap.org/data/2.5/"
+
+export const currentWeather = async (location: string) => {
+  try {
+    const res = await fetch(BASE_URL + "weather?q=" + location + "&APPID=" + API_KEY)
+    const weatherData = await res.json()
+    console.log(weatherData)
+    console.log(API_KEY)
+    return weatherData
+  } catch (err) {
+    console.error("Error fetching data", err);
+  }
+}
+
+export const forecastWeather = async (location: string) => {
+  try {
+    const res = await fetch(BASE_URL + "forecast/climate?q=" + location + "&APPID=" + API_KEY)
+    const forecastData = await res.json()
+    return forecastData
+  } catch (err) {
+    console.error("Error fetching data", err);
+  }
+}
+
