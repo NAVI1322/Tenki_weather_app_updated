0c6887be56e1a7476d88de5d51362a67a1575daa
Gurtaj06 <sonicgurtaj079@gmail.com>
2024-03-29 17:50:50 -0400

    made changes

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -25,7 +25,6 @@ export function InputBox() {
   return <div className=''>
     <div className="relative">
       <input type="text" className='outline-none p-3 w-96  text-left' onKeyDown={handleClick} onChange={handleChange} placeholder='Search your city..' />
-
     </div>
   </div>
 
