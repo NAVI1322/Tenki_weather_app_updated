257d2549111ce902a0c8a4176e189409f526dc36
NAVI1322 <rksharma041976@gmail.com>
2024-03-31 16:19:34 -0400

    inputbox

--- src/components/gridItems.tsx
+++ src/components/gridItems.tsx
@@ -38,7 +38,7 @@ export const GridItems = ({ climateData }: { climateData: ClimateData | null }) 
           :
           <div className=" rounded-xl bg-blue-50 flex flex-col p-6 ">
             <div className=" font-medium text-[18px] ">{"Today Temperatures"}</div>
-            <div className=" pt-1 grid grid-cols-2 gap-2">
+            <div className=" pt-1 grid grid-cols-2 gap-2 space-x-2">
               <div><p className=" text-slate-400">Morning</p>{climateData && Math.round(climateData?.list[0].temp.morn)}°C</div>
               <div><p className=" text-slate-400">Day</p>{climateData && Math.round(climateData?.list[0].temp.day)}°C</div>
               <div><p className=" text-slate-400">Eve</p>{climateData && Math.round(climateData?.list[0].temp.eve)}°C</div>
