4a9ce746f85a856d55980ee5e630a358082fc8ed
NAVI1322 <rksharma041976@gmail.com>
2024-04-01 17:56:34 -0400

    right card responsive

--- src/components/RightCard.tsx
+++ src/components/RightCard.tsx
@@ -36,74 +36,76 @@ export function RightCard(
   }
 
   return (
-    <div className="flex border-l-1 flex-col max-w-md items-center  pl-10 md:mt-0  mt-14 md:border-l h-screen ">
-      <div className="flex justify-between text-xl my-12 md:px-3" >This Week</div>
-      <div className="flex justify-start text-lg w-full font-medium">Today</div>
-      <div className="no-scrollbar overflow-x-auto flex flex-row gap-2 md:max-w-md max-md-sm bg-white mt-5 " >
+    <div className="flex border-l-1 flex-col max-w-md items-center  pl-5 md:mt-0  mt-14 md:border-l h-screen ">
+      <div className="flex justify-between text-xl   md:px-3" >This Week</div>
+      
+        <div className="no-scrollbar overflow-x-auto flex flex-row gap-4 md:max-w-md w-[90%] bg-white mt-5 " >
+          <div
 
-        <div className="flex flex-col items-center text-lg justify-between p-2 rounded-xl w-28 hover:bg-secondaryBlue cursor-pointer">
-          <div className="text-lg text-center mb-2 pr-6 pl-6">
-            {formatAMPM(new Date(currentData?.dt * 1000)) ? "Now" : ""}
+            className="flex flex-col items-center justify-between p-4 rounded-xl hover:bg-secondaryBlue cursor-pointer"
+          >
+            <div className="text-lg text-center mb-2 pr-6 pl-6">
+              {formatAMPM(new Date(currentData?.dt * 1000)) ? "Now" : ""}
+            </div>
+            <div className="text-center mb-2 ">
+              {fetchIcon(currentData ? currentData?.weather[0].icon : "")}
+            </div>
+            <div className="text-xl font-medium text-center pr-6 pl-6">
+              {Math.round(currentData ? currentData?.main.temp : 0)}°C
+            </div>
           </div>
-          <div className="text-center mb-2 ">
-            {fetchIcon(currentData ? currentData?.weather[0].icon : "")}
-          </div>
-          <div className="text-xl font-medium text-center pr-6 pl-6">
-            {Math.round(currentData ? currentData?.main.temp : 0)}°C
-          </div>
-        </div>
 
-        {hourlyData && (
-          <>
-            {hourlyData.list.map((hour: any, index: number) => (
-              <div
-                key={index}
-                className="flex flex-col items-center justify-between p-2 rounded-xl w-28 hover:bg-secondaryBlue cursor-pointer"
-              >
-                <div className="text-lg text-center mb-2 pr-6 pl-6">
-                  {formatAMPM(new Date(hour.dt * 1000))}
-                </div>
-                <div className="text-center mb-2 ">
-                  {fetchIcon(hour.weather[0].icon)}
-                </div>
-                <div className="text-xl font-medium text-center pr-6 pl-6">
-                  {Math.round(hourlyData.list[index].main.temp)}°C
-                </div>
-              </div>
-            ))}
-          </>
-        )}
-      </div>
-
-      <div className="text-neutral-800 flex flex-col w-full">
-
-        <div className="rounded-2xl justify-between mt-5" >
-          {climateData && (
-            <div className="flex flex-col space-y-4 ">
-              {climateData.list.map((day: any, index: number) => (
+          {hourlyData && (
+            <>
+              {hourlyData.list.map((hour: any, index: number) => (
                 <div
                   key={index}
-                  className="flex justify-between border-0 font-medium m-1"
+                  className="flex flex-col items-center justify-between p-4 rounded-xl hover:bg-secondaryBlue cursor-pointer"
                 >
-                  <div className="flex text-lg items-center min-w-24">
-                    {currentDay == new Date(day.dt * 1000).getDay()
-                      ? "Today"
-                      : dayNames[new Date(day.dt * 1000).getDay()]}
+                  <div className="text-lg text-center mb-2 pr-6 pl-6">
+                    {formatAMPM(new Date(hour.dt * 1000))}
                   </div>
-                  <div className="flex justify-center items-center">
-                    <div>{Math.round(day.temp.max)}° </div>{" "}
-                    <div className="text-gray-400">
-                      /{Math.round(day.temp.min)}°C
-                    </div>
+                  <div className="text-center mb-2 ">
+                    {fetchIcon(hour.weather[0].icon)}
                   </div>
-                  <div>{fetchIcon(day.weather[0].icon)}</div>
+                  <div className="text-xl font-medium text-center pr-6 pl-6">
+                    {Math.round(hourlyData.list[index].main.temp)}°C
+                  </div>
                 </div>
               ))}
-            </div>
+            </>
           )}
         </div>
-
+      
+      <div className="text-neutral-800 flex flex-col w-full">
+        
+          <div className="rounded-2xl justify-between mt-5" >
+            {climateData && (
+              <div className="flex flex-col space-y-4 ">
+                {climateData.list.map((day: any, index: number) => (
+                  <div
+                    key={index}
+                    className="flex justify-between border-0 font-medium m-1"
+                  >
+                    <div className="flex items-center">
+                      {currentDay == new Date(day.dt * 1000).getDay()
+                        ? "Today"
+                        : dayNames[new Date(day.dt * 1000).getDay()]}
+                    </div>
+                    <div className="flex justify-center items-center">
+                      <div>{Math.round(day.temp.max)}° </div>{" "}
+                      <div className="text-gray-400">
+                        /{Math.round(day.temp.min)}°C
+                      </div>
+                    </div>
+                    <div>{fetchIcon(day.weather[0].icon)}</div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+                  
       </div>
-    </div >
+    </div>
   );
 }
